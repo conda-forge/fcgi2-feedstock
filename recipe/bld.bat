69f5ebc2cb28da6ec1ca2ab26aed715186137782
Howard Butler <hobu.inc@gmail.com>
2024-08-20 11:27:57 -0500

    install libfcgi.lib for windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,6 +5,8 @@ copy cgi-fcgi\Release\cgi-fcgi.exe %PREFIX%\Library\bin
 if errorlevel 1 exit 1
 copy libfcgi\Release\libfcgi.dll %PREFIX%\Library\bin
 if errorlevel 1 exit 1
+copy libfcgi\Release\libfcgi.lib %PREFIX%\Library\lib
+if errorlevel 1 exit 1
 
 (robocopy include %PREFIX%\include "*.h") ^& IF %ERRORLEVEL% LEQ 1 exit 0
 
