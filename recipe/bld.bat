7c44f19c2a2dc466e3ca11bd6b90b83e4b6b4a90
Howard Butler <hobu.inc@gmail.com>
2024-08-20 10:06:46 -0500

    install libfcgi.dll in proper location

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,9 @@
 nmake /f Makefile.nt
 if errorlevel 1 exit 1
 
-copy cgi-fcgi\Release\cgi-fcgi.exe %PREFIX%\bin
+copy cgi-fcgi\Release\cgi-fcgi.exe %PREFIX%\Library\bin
 if errorlevel 1 exit 1
-copy libfcgi\Release\libfcgi.dll %PREFIX%\bin
+copy libfcgi\Release\libfcgi.dll %PREFIX%\Library\bin
 if errorlevel 1 exit 1
 
 (robocopy include %PREFIX%\include "*.h") ^& IF %ERRORLEVEL% LEQ 1 exit 0
